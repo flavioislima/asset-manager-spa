fee337540d4e53c889012974daa9424cc2d436b5
Flavio Lima <flaviolima@Flavios-iMac.local>
2018-07-16 15:44:05 -0300

    Added Axios and a Database to test Categories

--- src/Products.js
+++ src/Products.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
+import axios from 'axios'
 import ProdHome from './ProdHome'
 import Categories from './Categories'
 
 export default class Products extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            categories: []
+        }
+    }
+    componentDidMount() {
+        axios.get('http://localhost:3001/categories')
+            .then(result => {
+                this.setState({
+                    categories: result.data
+                })
+            })
+    }
+
+    renderCat(cat) {
+        return <li key={cat.id}><Link to={`/products/categories/${cat.id}`}>{cat.category}</Link></li>
+    }
+
     render() {
         const { match } = this.props
+        const { categories } = this.state
         return (
             <div className='row'>
                 <div className='col-md-2'>
                     <h3>Categories:</h3>
-                    <Link to='/products/categories/1'>Category 1 </Link>
+                    <ul>
+                        {categories.map(this.renderCat)}
+                    </ul>
                 </div>
                 <div className='col-md-10'>
                     <Route exact path={match.url} component={ProdHome}></Route>
