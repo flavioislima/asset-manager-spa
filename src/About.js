4058ee02605a7544e97d128eb60582d5504dcd96
Flavio Lima <flaviolima@Flavios-iMac.local>
2018-07-16 14:40:01 -0300

    Added Main Routes and components

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <h5>Created By <b>Flavio F Lima:</b></h5>
+                <a href="https://github.com/flavioislima"><h5>Github</h5></a>
+            </div>
+        )
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Products from './Products'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <div>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">Product Manager</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <Link className="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+                </li>
+                <li class="nav-item">
+                  <Link className="nav-link" to="/products">Products</Link>
+                </li>
+                <li class="nav-item">
+                  <Link className="nav-link" to="/about">About</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route exact path='/products' component={Products}></Route>
+        </div>
+      </Router>
     );
   }
 }
