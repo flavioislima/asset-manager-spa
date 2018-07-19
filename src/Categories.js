d06f5351993d9db522d1ccc1aaf0984ba716b8d7
Flavio Lima <flavioislima@gmail.com>
2018-07-19 17:52:55 -0300

    Added method to add products to a especific category

--- src/Categories.js
+++ src/Categories.js
@@ -10,10 +10,12 @@ export default class Categories extends Component {
         })
         this.loadData = this.loadData.bind(this)
         this.renderProd = this.renderProd.bind(this)
+        this.insertProd = this.insertProd.bind(this)
         this.deleteProd = this.deleteProd.bind(this)
     }
 
     url = 'http://localhost:3001/'
+    
 
     loadData(id) {
         axios.get(`${this.url}products?category=${id}`)
@@ -48,6 +50,25 @@ export default class Categories extends Component {
         return <h3 key={cat.id} >{cat.category}</h3>
     }
 
+    insertProd(event) {
+        const id = this.props.match.params.catId
+
+        if (event.keyCode === 13) {
+            axios
+                .post(`${this.url}products`,
+                    {
+                        "name": this.refs.newCat.value,
+                        "category": id
+                    })
+                .then(() => {
+                    // alert(`Product ${this.refs.newCat.value} created!`)
+                    this.refs.newCat.value = ''
+                    event.preventDefault()
+                    this.loadData(id)
+                })
+        }
+    }
+
     deleteProd(prod) {
         if (window.confirm(`Do you want to delete ${prod.name}?`)) {
             console.log(prod.id)
@@ -64,6 +85,12 @@ export default class Categories extends Component {
         return (
             <div>
                 <small style={{ textAlign: 'center' }} className="text-muted">{this.state.categories.map(this.renderCat)}</small>
+                <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text" id="inputGroup-sizing-default">Products</span>
+                </div>
+                <input onKeyDown={this.insertProd} ref="newCat" placeholder="Insert new Product" type="text" className="form-control" aria-label="New Category" aria-describedby="inputGroup-sizing-default" />
+            </div>
                 <ul className="list-group list-group-flush">
                     {this.state.products.map(this.renderProd)}
                 </ul>
